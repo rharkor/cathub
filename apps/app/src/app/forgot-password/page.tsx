811be29fc4f4b1309b75e6891f50e100a0de1b3f
rharkor <louis@huort.com>
2025-02-04 14:27:00 +0100

    fix: typecheck

--- apps/app/src/app/forgot-password/page.tsx
+++ apps/app/src/app/forgot-password/page.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image"
 import React from "react"
 
-import CatHubLogo from "/cathub.png"
-
 import FormForgotPassword from "./form-forgot-password"
 
 const ForgotPassword = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
-      <Image src={CatHubLogo} alt="" width={200} height={200} />
+      <Image src={"/cathub.png"} alt="logo" width={200} height={200} />
       <FormForgotPassword />
     </div>
   )
--- apps/app/src/app/login/page.tsx
+++ apps/app/src/app/login/page.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image"
 
-import CatHubLogo from "/cathub.png"
-
 import LoginForm from "./form-login"
 
 export default function LoginPage() {
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
-        <Image src={CatHubLogo} alt="" width={200} height={200} />
+        <Image src={"/cathub.png"} alt="logo" width={200} height={200} />
         <LoginForm />
       </div>
     </>
