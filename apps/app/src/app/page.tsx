d8e982897f12ef17104ed7d70d0dbc6ee8faf249
rharkor <louis@huort.com>
2025-02-04 12:07:38 +0100

    fix: turbo

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -3,10 +3,7 @@
 import { Button } from "@heroui/react"
 
 import { env } from "@/lib/env"
-import { trpc } from "@/lib/trpc/client"
 
 export default function Home() {
-  const { data } = trpc.me.test.useQuery()
-  console.log(data)
   return <Button>Test env {env.NEXT_PUBLIC_ENV}</Button>
 }
