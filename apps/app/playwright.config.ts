3832a6425e506da0888525624bf2dbf3529e8830
Marco Da Silva <marco.dasilva@ynov.com>
2025-02-25 10:55:57 +0100

    feat: add github CI and POM

--- apps/app/playwright.config.ts
+++ apps/app/playwright.config.ts
@@ -15,7 +15,7 @@ import { defineConfig, devices } from "@playwright/test"
 export default defineConfig({
   testDir: "./src",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -30,7 +30,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
