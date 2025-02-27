07c8fb89de8337841230a53aa051464c83532ff2
rharkor <louis@huort.com>
2025-02-27 14:44:21 +0100

    refactor: easter egg

--- apps/app/src/components/easter-egg.tsx
+++ apps/app/src/components/easter-egg.tsx
@@ -30,6 +30,7 @@ export function EasterEgg() {
       <ModalContent>
         <ModalHeader>Oups la boulette</ModalHeader>
         <ModalBody>
+          <Image src="/goat.png" alt="Le goat" className="w-full max-w-md rounded-md" width={500} height={375} />
           <Image
             src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmM0ZWJ4YTN0ZjIxdmRudmlveXY1M2RkMjJiOGdjMWM3cm9jamtnbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QAgG5sDYr7sbC9tYDB/giphy.gif"
             alt="Oups la boulette"
